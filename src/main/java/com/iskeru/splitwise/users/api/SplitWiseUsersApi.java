09f0b457809573bd6886c2aade747758f4670d58
Moacyr Ricardo <mpereira@quintoandar.com.br>
2023-03-05 12:25:12 -0300

    removing unnecessary pkgs

--- src/main/java/com/iskeru/splitwise/users/api/SplitWiseUsersApi.java
+++ src/main/java/com/iskeru/splitwise/users/api/SplitWiseUsersApi.java
@@ -1,20 +1,13 @@
 package com.iskeru.splitwise.users.api;
 
-import java.util.Date;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.iskeru.splitwise.expenses.model.SplitWiseExpense;
 
 @Path("/api/v3.0/")
 @Consumes({ MediaType.APPLICATION_JSON })
@@ -23,6 +16,6 @@ public interface SplitWiseUsersApi {
 
 	@GET
 	@Path("/get_current_user")
-	public SplitWiseUserResponse getExpenses(@HeaderParam(HttpHeaders.AUTHORIZATION) String apiKey);
+	public SplitWiseUserResponse getCurrentUser(@HeaderParam(HttpHeaders.AUTHORIZATION) String apiKey);
 
 }
