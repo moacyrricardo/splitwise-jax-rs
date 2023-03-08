66f649ea840a772be8a5307f3c2f9212acc3bae5
Moacyr Ricardo <mpereira@quintoandar.com.br>
2023-03-08 20:07:51 -0300

    Mapping errors

--- src/main/java/com/iskeru/splitwise/expenses/api/SplitWiseExpensesResponse.java
+++ src/main/java/com/iskeru/splitwise/expenses/api/SplitWiseExpensesResponse.java
@@ -1,6 +1,7 @@
 package com.iskeru.splitwise.expenses.api;
 
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.iskeru.splitwise.expenses.model.SplitWiseExpense;
@@ -15,4 +16,6 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SplitWiseExpensesResponse {
 	private List<SplitWiseExpense> expenses;
+	
+	private Map<String, Object> errors;
 }
