1c11c41fe6f62aa6bd14f417c78f1d5e4d9a99e3
Moacyr Ricardo <mpereira@quintoandar.com.br>
2023-03-09 12:01:47 -0300

    Extracting date format to constants

--- src/main/java/com/iskeru/splitwise/utils/SplitWiseConstants.java
+++ src/main/java/com/iskeru/splitwise/utils/SplitWiseConstants.java
@@ -5,6 +5,8 @@ public class SplitWiseConstants {
 	public static final String API_URL = "https://secure.splitwise.com";
 	
 	public static final Long NO_GROUP = 0l;
+	
+	public static final String DATE_PATTERN = "yyyy-MM-dd'T'hh:mm:ss'Z'";
 
 	private SplitWiseConstants() {
 		throw new IllegalArgumentException("No instanciatable");
